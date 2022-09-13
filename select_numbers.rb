c4cef9ab4ab040ae27fa0bdae387b1017fc34457
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-09-12 20:21:28 -0700

    adds select_numbers solution

---
+++ select_numbers.rb
@@ -0,0 +1,29 @@
+# Given an array of numbers, write a function that returns a new array in which only select numbers from the original array are included, based on the following details:
+
+# The new array should always start with the first number from the original array. The next number that should be included depends on what the first number is. The first number dictates how many spaces to the right the computer should move to pick the next number. For example, if the first number is 2, then the next number that the computer should select would be two spaces to the right. This number gets added to the new array. If this next number happens to be a 4, then the next number after that is the one four spaces to the right. And so on and so forth until the end of the array is reached.
+
+# Input:
+# [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+
+# Output:
+# [2, 3, 1, 2, 2, 1, 5, 2, 2]
+
+
+# set variable index to 0
+# set variable new_array to empty array
+# start with array[0], add that number to new_array
+# add array[0] to index
+# index should now be set to 2, based on example array above.
+# all inside while loop
+
+def select(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << array[i]
+    i += array[i]
+  end
+  return new_array
+end
+
+p select([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
